210e9ad42b6c1ce588ba8a0eb7a011a310188d1e
Neminao <nemanja.nedeljkovic@mail.com>
2019-07-29 17:21:54 +0200

    Updates

---
+++ precache-manifest.7e10a86e1608a4aeed2b30593aa09802.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb2d0a2cdbc3a01885a964f1e1568b03",
+    "url": "/Tetris/static/media/computer-3163437.fb2d0a2c.png"
+  },
+  {
+    "revision": "9708a83805d881b3530f",
+    "url": "/Tetris/static/js/runtime~main.e25822d4.js"
+  },
+  {
+    "revision": "ec6004c9186db8368a08",
+    "url": "/Tetris/static/js/main.fee79890.chunk.js"
+  },
+  {
+    "revision": "9be37b0c314ae9c6a477",
+    "url": "/Tetris/static/js/2.8f5e563c.chunk.js"
+  },
+  {
+    "revision": "ec6004c9186db8368a08",
+    "url": "/Tetris/static/css/main.33906888.chunk.css"
+  },
+  {
+    "revision": "323f94cadaf4860894da613234c8190c",
+    "url": "/Tetris/index.html"
+  }
+];
